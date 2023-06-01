f5a634196259d934fda1bc2057ea015458cd29c2
aletanus <alessandro@altadesign.com.br>
2023-06-01 09:50:18 -0300

    Cors to localhost:3000 added.

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://localhost:3000'
+  });
   const config = new DocumentBuilder()
   .setTitle('Your Contacts App')
   .setDescription(' Stay Connected: Your Ultimate Contacts Companion ')
